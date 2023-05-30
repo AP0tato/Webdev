257d62f258e694c5a019a2fe31d2834cd2668929
AP0tato <aliabid0207@gmail.com>
2023-05-29 22:59:52 -0400

    More code

--- Memory/script.js
+++ Memory/script.js
@@ -1,4 +1,8 @@
+var lives = 3, time = 60, score = 0, speed = 1, diff = 1;
+
+var life = document.getElementsByClassName("lives"), scor = document.getElementsByClassName("score"), tim = document.getElementsByClassName("time");
+
 function start() 
 {
-
+    document.getElementsByClassName("lives").innerHTML = "Lives: " + lives;
 }
