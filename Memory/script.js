e20a2d0b6842fecda495c86f69d7621facf6ae6e
AP0tato <aliabid0207@gmail.com>
2023-05-30 21:53:17 -0400

    Finished memory game

--- Memory/script.js
+++ Memory/script.js
@@ -1,4 +1,4 @@
-var lives = 3, time = 3, score = 0, speed = 2, diff = 1;
+var lives = 3, score = 0, speed = 1.7, diff = 1;
 var pattern = [], input = [];
 var keys = {1: "u", 2: "r", 3:"d", 4:"l"};
 const sleep = (ms) => {
@@ -7,12 +7,26 @@ const sleep = (ms) => {
 
 async function start() 
 {
+    lives = 3;
+    time = 3;
+    speed = 2;
+    diff = 1;
+    score = 0;
+
+    update();
     while(lives>0) {
+        update();
         generate();
         await display();
-        check();
-        await sleep(3000);
+        await check();
     }
+    update();
+}
+
+function update() {
+    document.getElementById("lives").innerHTML = "Lives: "+lives;
+    document.getElementById("score").innerHTML = "Score: "+score;
+    document.getElementById("diff").innerHTML = "Pattern length: "+diff;
 }
 
 function generate() {
@@ -27,39 +41,38 @@ async function display() {
         temp.style.backgroundColor='#000000';
         await sleep(speed*1000);
         temp.style.backgroundColor='#FFFFFF';
-        await sleep(speed*250);
+        await sleep(speed*150);
     }
     return new Promise((resolve, reject) => { resolve(); })
 }
 
 async function check() {
     input = [];
-    await sleep(speed*1000);
+    await sleep(diff*1000);
     console.log(pattern);
     console.log(input);
     if(input.length==pattern.length) {
         for(let i = 0; i < input.length; i++) {
             if(input[i]!=pattern[i]) {
                 lives--;
-                return;
+                return new Promise((resolve, reject) => { resolve(); })
             }
         }
     }
     else {
         lives--;
-        return;
+        return new Promise((resolve, reject) => { resolve(); })
     }
-    
+
     correct();
+    return new Promise((resolve, reject) => { resolve(); })
 }
 
 function correct() {
-    score = score * (speed + diff);
+    score = score + 25;
 
     diff++;
     speed -= 0.1;
-
-    time = parseInt(parseInt(speed+time)*1.7);
 }
 
 function changeText(type, id, text) {
@@ -70,12 +83,35 @@ function changeText(type, id, text) {
 }
 
 document.addEventListener('keydown', function(event) {
-    if(event.keyCode==37) 
+    if(event.keyCode==37) {
         input.push(4); // Left
-    if(event.keyCode==38) 
+        document.getElementById("l1").style.backgroundColor = "#000000";
+    }
+    if(event.keyCode==38) { 
         input.push(1); // Up
-    if(event.keyCode==39) 
+        document.getElementById("u1").style.backgroundColor = "#000000";
+    }
+    if(event.keyCode==39) { 
         input.push(2); // Right
-    if(event.keyCode==40) 
+        document.getElementById("r1").style.backgroundColor = "#000000";
+    }
+    if(event.keyCode==40) { 
         input.push(3); // Down
+        document.getElementById("d1").style.backgroundColor = "#000000";
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if(event.keyCode==37) 
+        document.getElementById("l1").style.backgroundColor = "#FFFFFF";
+    
+    if(event.keyCode==38) 
+        document.getElementById("u1").style.backgroundColor = "#FFFFFF";
+    
+    if(event.keyCode==39) 
+        document.getElementById("r1").style.backgroundColor = "#FFFFFF";
+    
+    if(event.keyCode==40) 
+        document.getElementById("d1").style.backgroundColor = "#FFFFFF";
+    
 });
